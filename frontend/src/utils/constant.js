83ade827e195d5fe6ea115e3b069e25d4661c342
Mohd Anas <146380903+thorfinn99@users.noreply.github.com>
2025-01-30 12:43:10 +0530

    Update constant.js

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -1,3 +1,3 @@
-export const USER_API_END_POINT = "guest-house-react.vercel.app/user";
-export const ROOM_API_END_POINT = "guest-house-react.vercel.app/room";
-export const BOOKING_API_END_POINT = "guest-house-react.vercel.app/bookings";
+export const USER_API_END_POINT = "https://guest-house-react.vercel.app/user";
+export const ROOM_API_END_POINT = "https://guest-house-react.vercel.app/room";
+export const BOOKING_API_END_POINT = "https://guest-house-react.vercel.app/bookings";
