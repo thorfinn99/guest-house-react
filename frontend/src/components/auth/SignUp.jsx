abcc1b06c466bf0e09d8f8605c450c44d59cc04d
Mohd Anas <146380903+thorfinn99@users.noreply.github.com>
2025-08-07 12:17:56 +0530

    Update SignUp.jsx

--- frontend/src/components/auth/SignUp.jsx
+++ frontend/src/components/auth/SignUp.jsx
@@ -192,7 +192,7 @@ function SignUp() {
                   </div>
                   <div className="flex items-center">
                     <input
-                      id="inline-radio"
+                      id="owner-radio"
                       type="radio"
                       value="owner"
                       name="role"
@@ -201,10 +201,10 @@ function SignUp() {
                       className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
                     />
                     <label htmlFor="inline-radio" className="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">
-                      Guest
+                      Owner
                     </label>
                   </div>
-                  
+                  //////////////////
                 </div>
 
                 <div className="flex items-center gap-2">
