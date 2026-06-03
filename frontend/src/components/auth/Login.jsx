b9b08d9bda5b22bf1e925d452eb5116ae2ee4dd2
Mohd Anas <146380903+thorfinn99@users.noreply.github.com>
2026-06-03 07:10:40 +0530

    Change login header text from 'MJPRU' to 'Stay Ease'

--- frontend/src/components/auth/Login.jsx
+++ frontend/src/components/auth/Login.jsx
@@ -55,7 +55,7 @@ function Login() {
             <section className="bg-white dark:bg-gray-900">
                 <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
                     <a href="#" className="flex text-3xl items-center mb-6 font-semibold text-gray-900 dark:text-white">
-                        <Link to="/"><p>MJPRU<span className="text-orange-500"> GUEST HOUSE</span></p></Link>
+                        <Link to="/"><p>Stay<span className="text-orange-500"> Ease</span></p></Link>
                     </a>
                     <div className="w-full max-w-md bg-white rounded-lg shadow-xl dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
                         <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
